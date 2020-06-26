6155f89e73b72804b78304be87a7c8397855b2da
skysky00 <moumens@gmail.com>
2020-06-26 21:57:17 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    document.addEventListener('click', event => {
+      console.log(event);
+    });
+
+    let counter = 1000;
+    setInterval(() => {
+      console.log(counter);
+      counter++;
+    },10000);
+
+    setTimeout(() => {
+      console.log("finish");
+    }, 1000);
+  }
 }
